ee3d13464e3d90acf18dc6006af438b1ccf1b5d9
Sary Elmelegy <arch.selmelegy@gmail.com>
2018-10-01 22:29:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# MIT_6.006_Introduction_to_Algorithms
---
+++ peak_finding/linear_peak.py
@@ -0,0 +1,16 @@
+lst = [22, 35, 62, 84, 12, 25, 39, 99, 152, 124]
+
+
+# A function that makes a linear search 
+# The worst case time complexity of this method would be O(n).
+def peak(lst):
+    
+    for i in range(1, len(lst) - 1):
+        if(lst[i] >= lst[i - 1] and lst[i] >= lst[i + 1]):
+            return lst[i]
+        
+print("The first peak of the list is: " + str(peak(lst)))
+
+
+# A function that uses a Divide and conquer method to find a peak
+# The worst case time complexity of this method would be O(log n).
